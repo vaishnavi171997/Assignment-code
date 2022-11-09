1b7eb8d3917d9382d10c4b186e45562044181270
vaishnavi171997 <116464260+vaishnavi171997@users.noreply.github.com>
2022-11-09 22:36:43 +0530

    Importing pandas and matplotlib packages
    Importing Pandas package to read the file
    Importing matplotlib python package for data plotting and visualization

---
+++ Function code-copy-spyder.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  9 21:13:15 2022
+
+@author: HP
+"""
+
+#Importing Pandas package to read the file 
+import pandas as pd
+#Importing matplotlib python package for data plotting and visualization
+import matplotlib.pyplot as plt
