dcd6018f454d687f018e71e199859174c99ebecc
vaishnavi171997 <116464260+vaishnavi171997@users.noreply.github.com>
2022-11-09 22:46:40 +0530

    Defining columns and reading the file

--- Function code-copy-spyder.py
+++ Function code-copy-spyder.py
@@ -9,3 +9,7 @@ Created on Wed Nov  9 21:13:15 2022
 import pandas as pd
 #Importing matplotlib python package for data plotting and visualization
 import matplotlib.pyplot as plt
+
+#defining columns and reading the file
+columns = ['score1','score2','prob1','prob2']
+matches = pd.read_csv('C:\\Users\\HP\\Downloads\\applieddsassignment\\wwc_matches.csv',nrows=4,usecols=columns)
